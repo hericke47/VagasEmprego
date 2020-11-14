52b4aebc891c14742db7fb57dacaa7660165fa92
Herick Exterkoetter <herickherick47@gmail.com>
2020-11-14 10:09:01 -0300

    :bento: chore: adding the formulary and test the post request

--- cadastrar.php
+++ cadastrar.php
@@ -2,6 +2,8 @@
 
 require __DIR__.'/vendor/autoload.php'; // para chamar classes
 
+print_r($_POST);
+
 include __Dir__.'/includes/header.php';
-include __Dir__.'/includes/listagem.php';
+include __Dir__.'/includes/formulario.php';
 include __Dir__.'/includes/footer.php';
