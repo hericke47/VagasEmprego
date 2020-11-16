5278e5d1c45f36864e98e64e7a6d6c772ea18898
Herick Exterkoetter <herickherick47@gmail.com>
2020-11-15 23:30:21 -0300

    :bento: feat: finished the crud using PDO

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 require __DIR__.'/vendor/autoload.php'; // para chamar classes
 
+use \App\Entity\Vaga;
+
+$vagas = Vaga::getVagas();
+
 include __Dir__.'/includes/header.php';
 include __Dir__.'/includes/listagem.php';
 include __Dir__.'/includes/footer.php';
